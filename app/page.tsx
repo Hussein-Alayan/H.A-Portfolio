c2f14d2e9513130c3d512eb8a25a1ed7661a7845
Hussein Alayan <Alayen.hussein@gmail.com>
2025-11-02 19:57:08 +0200

    Add EducationSection component to Home layout

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <SkillsSection />
+      <EducationSection />
       <ProjectSection />
       <VolunteerSection />
       <ContactSection />
